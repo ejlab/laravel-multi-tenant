95f4d255b3b632c649ad3621cfa20ab7cde920ff
박은준 <ej@sellmate.co.kr>
2017-07-07 15:53:23 +0900

    Tenant 모델 SoftDeletes 정의 누락 버그 수정

--- database/Tenant.php
+++ database/Tenant.php
@@ -3,10 +3,14 @@
 namespace App\Models\System;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tenant extends Model
 {
+    use SoftDeletes;
+
     protected $connection = 'system';
     protected $guarded = [];
     protected $casts = ['configs' => 'json'];
+    protected $dates = ['deleted_at'];
 }
