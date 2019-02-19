b91075d85a2ca678de108c2512508640d6d0449a
임문혁 <moonhyuk@sellmate.co.kr>
2019-02-19 16:17:45 +0900

    남아있던 fire 명령어 수정

--- src/Commands/Seeds/SeedCommand.php
+++ src/Commands/Seeds/SeedCommand.php
@@ -16,7 +16,7 @@ class SeedCommand extends BaseCommand
      *
      * @return void
      */
-    public function fire()
+    public function handle()
     {
         if (! $this->confirmToProceed()) return;
 
